48656aec166de9365a62a7e429bc39529ca9cfcc
johnberroa <johnberroa@gmail.com>
2018-04-14 18:41:13 +0200

    Final push before submit

--- keras/keras_finger_HMF.py
+++ keras/keras_finger_HMF.py
@@ -8,106 +8,107 @@ import cv2
 
 from sklearn.model_selection import train_test_split as split_data
 from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Conv2D, Flatten, MaxPool2D, Dense, Dropout
+from tensorflow.python.keras.layers import Conv2D, Flatten, MaxPool2D, Dense, Dropout, BatchNormalization
 from tensorflow.python.keras.utils import to_categorical
+from tensorflow.python.keras.callbacks import ModelCheckpoint
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
 
 
 # Boolean to tell the program to resize the images to a smaller resolution
 RESIZE = False
-print("RESIZE:", RESIZE)
-# Hyperparameters as per 'How Many Fingers'
-# https://github.com/jgv7/CNN-HowManyFingers/blob/master/trainModel.ipynb
-EPOCHS = 500
+print("RESIZE to 200", RESIZE)
+print("MORE LAYERS")
+# Hyperparameters at top so that they are easily changed in vim
+EPOCHS = 200
 BATCHSIZE = 128
+print("BATCHSIZE:", BATCHSIZE)
+
+if RESIZE:
+    size = (200, 200)
+else:
+    size = (300, 300)
 
 
-def load_data(resize):
-	"""
-	Data loading from the tmp folder
-	"""
-	start = time.time()
-	if resize:
-		images = np.zeros((2110 * 2, 50, 50, 3))
-	else:
-		images = np.zeros((2110 * 2, 300, 300, 3))
-	labels = np.zeros((2110 * 2, 1))
-
-	print("Loading data...")
-	os.chdir('./tmp')
-	contents = os.listdir('./')
-	for i, entry in enumerate(contents):
-		img = cv2.imread(entry)
-		if resize:
-			images[i] = cv2.resize(img, (50,50))
-		else:
-			images[i] = img
-		labels[i] = int(entry[0])
-	os.chdir('..')
-	images, labels = preprocess_data(images, labels)
-	end = time.time()
-	print("Data loaded. ({} mins)".format((end - start) / 60))
-	return images, labels
+def feature_extract(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.GaussianBlur(img, (7,7), 3)
+    img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+    ret, new = cv2.threshold(img, 25, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    return new
 
 
-def preprocess_data(imgs, lbls):
-	imgs = imgs / 255 # normalize between 0 and 1
-	# Flip all images (they will be randomly shuffled when split)
-	for i in range(2110):
-		imgs[i + 2110] = np.fliplr(imgs[i])
-		lbls[i + 2110] = lbls[i]
-	return imgs, lbls
+train_datagen = ImageDataGenerator( rescale=1./255,
+                                    rotation_range=10.,
+                                    width_shift_range=0.1,
+                                    height_shift_range=0.1,
+                                    zoom_range=0.2,
+                                    horizontal_flip=True,
+                                    zca_whitening=True,
+                                    preprocessing_function=feature_extract
+                                  )
+
+valid_datagen  = ImageDataGenerator(rescale=1./255, zca_whitening=True)  # maybe no whitening on testing sets?
+
+train_gen = train_datagen.flow_from_directory(
+        'data/train/',
+        target_size=size,
+        color_mode='rgb',
+        batch_size=BATCHSIZE,
+        classes=['1','2','3','4','5'],
+        class_mode='categorical'
+    )
+
+valid_gen = valid_datagen.flow_from_directory(
+        'data/val/',
+        target_size=size,
+        color_mode='rgb',
+        batch_size=BATCHSIZE,
+        classes=['1','2','3','4','5'],
+        class_mode='categorical'
+    )
 
 
-def split(images, labels):
-	"""
-	Splits images and labels into training, validation, and test sets.
-	"""
-	train, valtest, labels, v_labels = split_data(images, labels,
-															shuffle=True, train_size=.6,
-															test_size=.4)
-	validation, test, v_labels, t_labels = split_data(valtest, v_labels,
-															shuffle=True,
-															train_size=.5,
-															test_size=.5)
-	return train, validation, test, labels, v_labels, t_labels
-
-
-# Load the data and split into sets
-images, labels = load_data(RESIZE)
-train, validation, test, labels, v_labels, t_labels = split(images, labels)
 
 # Create proper input dimensions
 if RESIZE:
-	input_dims = (50, 50, 3)
+    input_dims = (200, 200, 3)
 else:
-	input_dims = (300, 300, 3)
+    input_dims = (300, 300, 3)
 
 
 # Creation of the model
 model = Sequential()
-model.add(Conv2D(32, (3,3), activation='relu', input_shape=input_dims))
-model.add(MaxPool2D((2,2)))
-model.add(Conv2D(64, (3,3), activation='relu'))
-model.add(MaxPool2D((2,2)))
-model.add(Conv2D(128, (3,3), activation='relu'))
-model.add(MaxPool2D((2,2)))
-model.add(Conv2D(128, (3,3), activation='relu'))
-model.add(MaxPool2D((2,2)))
+# model.add(Conv2D(32, (3,3), activation='relu', input_shape=input_dims))
+# model.add(MaxPool2D((2,2)))
+# model.add(Conv2D(64, (3,3), activation='relu'))
+# model.add(MaxPool2D((2,2)))
+# model.add(Conv2D(128, (3,3), activation='relu'))
+# model.add(MaxPool2D((2,2)))
+# model.add(Conv2D(128, (3,3), activation='relu'))
+model.add(MaxPool2D((10,10)))
 model.add(Flatten())
-model.add(Dense(512, activation='relu'))
-model.add(Dropout(0.3))
+# model.add(Dense(512, activation='relu'))
+# model.add(Dropout(0.3))
 model.add(Dense(5, activation='softmax'))
 
 model.compile(optimizer='adadelta', loss='categorical_crossentropy', metrics=['acc'])
 
-one_hots = to_categorical(labels-1, num_classes=5)  # due to out of bounds error, must be labels - 1 to make it start at 0
-one_hots_v = to_categorical(v_labels-1, num_classes=5)
+callbacks_list = [ModelCheckpoint(filepath='weights.{epoch:02d}-{val_loss:.2f}.hdf5', monitor='val_loss', save_best_only=True)]
 
-hist = model.fit(train, one_hots, epochs=EPOCHS, batch_size=BATCHSIZE, validation_data=(validation, one_hots_v), verbose=0)
+hist = model.fit_generator(
+        train_gen,
+        steps_per_epoch=200//BATCHSIZE,  # len(train) / batchsize
+        epochs=EPOCHS,
+        validation_data=valid_gen,
+        validation_steps=100//BATCHSIZE, # same as above
+        callbacks=callbacks_list,
+        verbose=2
+    )
+
 
 # Save the training data into npy save files so that they can be plotted separately
-np.save('HMF-t-acc.npy', hist.history['acc'])
-np.save('HMF-t-loss.npy', hist.history['loss'])
-np.save('HMF-v-acc.npy', hist.history['val_acc'])
-np.save('HMF-v-loss.npy', hist.history['val_loss'])
+np.save('HMF-tb{}-acc.npy'.format(BATCHSIZE), hist.history['acc'])
+np.save('HMF-tb{}-loss.npy'.format(BATCHSIZE), hist.history['loss'])
+np.save('HMF-vb{}-acc.npy'.format(BATCHSIZE), hist.history['val_acc'])
+np.save('HMF-vb{}-loss.npy'.format(BATCHSIZE), hist.history['val_loss'])
 print("FINAL ACCURACY:", hist.history['acc'][-1])
--- keras/keras_finger_LargeCNN.py
+++ keras/keras_finger_LargeCNN.py
@@ -10,74 +10,62 @@ from sklearn.model_selection import train_test_split as split_data
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Conv2D, Flatten, MaxPool2D, Dense, Dropout, BatchNormalization
 from tensorflow.python.keras.utils import to_categorical
+from tensorflow.python.keras.callbacks import ModelCheckpoint
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
 
 
 # Boolean to tell the program to resize the images to a smaller resolution
 RESIZE = False
-print("RESIZE", RESIZE)
+print("RESIZE to 200", RESIZE)
+print("MORE LAYERS")
 # Hyperparameters at top so that they are easily changed in vim
-EPOCHS = 500
-BATCHSIZE = 32
+EPOCHS = 200
+BATCHSIZE = 100
+print("BATCHSIZE:", BATCHSIZE)
+
+if RESIZE:
+	size = (200, 200)
+else:
+	size = (300, 300)
 
 
-def load_data(resize):
-	"""
-	Data loading from the tmp folder
-	"""
-	start = time.time()
-	if resize:
-		images = np.zeros((2110 * 2, 50, 50, 3))
-	else:
-		images = np.zeros((2110 * 2, 300, 300, 3))
-	labels = np.zeros((2110 * 2, 1))
+train_datagen = ImageDataGenerator( rescale=1./255,
+                                    rotation_range=10.,
+                                    width_shift_range=0.1,
+                                    height_shift_range=0.1,
+                                    zoom_range=0.2,
+                                    horizontal_flip=True,
+                                    zca_whitening=True
+                                  )
 
-	print("Loading data...")
-	os.chdir('./tmp')
-	contents = os.listdir('./')
-	for i, entry in enumerate(contents):
-		img = cv2.imread(entry)
-		if resize:
-			images[i] = cv2.resize(img, (50,50))
-		else:
-			images[i] = img
-		labels[i] = int(entry[0])
-	os.chdir('..')
-	images, labels = preprocess_data(images, labels)
-	end = time.time()
-	print("Data loaded. ({} mins)".format((end - start) / 60))
-	return images, labels
+valid_datagen  = ImageDataGenerator(rescale=1./255, zca_whitening=True)  # maybe no whitening on testing sets?
 
+train_gen = train_datagen.flow_from_directory(
+        'data/train/',
+        target_size=size,
+        color_mode='color',
+        batch_size=BATCHSIZE,
+        classes=['1','2','3','4','5'],
+        class_mode='categorical',
+        save_to_dir='augmented',  # SAVING THE IMAGES TO VISUALIZE WHAT THE AUGMENTATION IS DOING
+        save_prefix='AUG',
+		save_format="jpeg"
+    )
 
-def preprocess_data(imgs, lbls):
-	imgs = imgs / 255 # normalize between 0 and 1
-	# Flip all images (they will be randomly shuffled when split)
-	for i in range(2110):
-		imgs[i + 2110] = np.fliplr(imgs[i])
-		lbls[i + 2110] = lbls[i]
-	return imgs, lbls
+valid_gen = test_datagen.flow_from_directory(
+        'data/val/',
+        target_size=size,
+        color_mode='color',
+        batch_size=BATCHSIZE,
+        classes=['1','2','3','4','5'],
+        class_mode='categorical'
+    )
 
 
-def split(images, labels):
-	"""
-	Splits images and labels into training, validation, and test sets.
-	"""
-	train, valtest, labels, v_labels = split_data(images, labels,
-															shuffle=True, train_size=.6,
-															test_size=.4)
-	validation, test, v_labels, t_labels = split_data(valtest, v_labels,
-															shuffle=True,
-															train_size=.5,
-															test_size=.5)
-	return train, validation, test, labels, v_labels, t_labels
-
-
-# Load the data and split into sets
-images, labels = load_data(RESIZE)
-train, validation, test, labels, v_labels, t_labels = split(images, labels)
 
 # Create proper input dimensions
 if RESIZE:
-	input_dims = (50, 50, 3)
+	input_dims = (200, 200, 3)
 else:
 	input_dims = (300, 300, 3)
 
@@ -85,33 +73,57 @@ else:
 # Creation of the model
 # Only 3 Conv sections because max pool would make the images too small for any useful analysis
 model = Sequential()
-model.add(Conv2D(16, (3, 3), input_shape=input_dims, padding='same', activation='relu'))
+model.add(Conv2D(16, (3, 3), input_shape=input_dims, padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01),
+                activity_regularizer=regularizers.l1(0.01)))
 model.add(BatchNormalization())
 model.add(MaxPool2D((2,2)))
-model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
+model.add(Conv2D(32, (3, 3), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01),
+                activity_regularizer=regularizers.l1(0.01)))
 model.add(BatchNormalization())
 model.add(MaxPool2D((2,2)))
-model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
+model.add(Conv2D(32, (3, 3), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01),
+                activity_regularizer=regularizers.l1(0.01)))
+model.add(BatchNormalization())
+model.add(MaxPool2D((2,2)))
+model.add(Conv2D(64, (3, 3), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01),
+                activity_regularizer=regularizers.l1(0.01)))
+model.add(BatchNormalization())
+model.add(MaxPool2D((2,2)))
+model.add(Conv2D(16, (3, 3), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.01),
+                activity_regularizer=regularizers.l1(0.01)))
 model.add(BatchNormalization())
 model.add(Flatten())
-model.add(Dense(512, activation='relu'))
+model.add(Dense(512, activation='relu', activity_regularizer=regularizers.l1(0.01)))
 model.add(Dropout(.5))
-model.add(Dense(128, activation='relu'))
+model.add(Dense(128, activation='relu', activity_regularizer=regularizers.l1(0.01)))
 model.add(Dropout(.5))
-model.add(Dense(5, activation='softmax'))
+model.add(Dense(5, activation='softmax', activity_regularizer=regularizers.l1(0.01)))
 
 model.compile(optimizer='adam',
 			  loss='categorical_crossentropy',
 			  metrics=['accuracy'])
 
-one_hots = to_categorical(labels-1, num_classes=5)  # due to out of bounds error, must be labels - 1 to make it start at 0
-one_hots_v = to_categorical(v_labels-1, num_classes=5)
+#one_hots = to_categorical(labels-1, num_classes=5)  # due to out of bounds error, must be labels - 1 to make it start at 0
+#one_hots_v = to_categorical(v_labels-1, num_classes=5)
 
-hist = model.fit(train, one_hots, epochs=EPOCHS, batch_size=BATCHSIZE, validation_data=(validation, one_hots_v), verbose=0)
+#hist = model.fit(train, one_hots, epochs=EPOCHS, batch_size=BATCHSIZE, validation_data=(validation, one_hots_v), verbose=2)
+callbacks_list = [ModelCheckpoint(filepath='weights.{epoch:02d}-{val_loss:.2f}.hdf5', monitor='val_loss', save_best_only=True)]
+
+
+hist = model.fit_generator(
+        train_gen,
+        steps_per_epoch=200//BATCHSIZE,  # len(train) / batchsize
+        epochs=EPOCHS,
+        validation_data=valid_gen,
+        validation_steps=100//BATCHSIZE, # same as above
+        callbacks=callbacks_list,
+        verbose=2
+    )
+
 
 # Save the training data into npy save files so that they can be plotted separately
-np.save('tL-acc.npy', hist.history['acc'])
-np.save('tL-loss.npy', hist.history['loss'])
-np.save('vL-acc.npy', hist.history['val_acc'])
-np.save('vL-loss.npy', hist.history['val_loss'])
-print("FINAL ACCURACY:", hist.history['acc'][-1])
+np.save('tLb{}-acc.npy'.format(BATCHSIZE), hist.history['acc'])
+np.save('tLb{}-loss.npy'.format(BATCHSIZE), hist.history['loss'])
+np.save('vLb{}-acc.npy'.format(BATCHSIZE), hist.history['val_acc'])
+np.save('vLb{}-loss.npy'.format(BATCHSIZE), hist.history['val_loss'])
+print("FINAL ACCURACY:".format(BATCHSIZE), hist.history['acc'][-1])
